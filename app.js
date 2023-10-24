ea055758a915280783a869142c74679997afedbd
Elena Adamova <ku4eryabl@gmail.com>
2023-10-23 21:57:55 -0400

    adding js effects

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log("I am in the HTML");
+alert("Welcome to my page!");
+
+const usersName = prompt("What is your name?");
+console.log(usersName);
+
+const city = prompt("Where are you from?");
+console.log(city);
+
+
+// string concatenation
+document.write("Hi " + usersName + " from " + city + "! Welcome to page about my city!");
+
+// if (this is true) {execute this code}
+
+if (usersName == 'Kassie'){
+    alert('Hiya teach!');
+}
+else {
+    alert('Glad to have you here!');
+}
+
+
+
